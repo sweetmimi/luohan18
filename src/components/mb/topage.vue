ec4dbc31898ee6155c5306ec9a9ebc71226f7c00
zys <zys@firestorm-sea.com>
2021-01-29 18:55:54 +0800

    提交

---
+++ src/components/mb/topage.vue
@@ -0,0 +1,34 @@
+<!-- zys -->
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name:'ToPage' ,
+  components: {},
+
+  data () {
+    return {
+    };
+  },
+
+ async created(){
+    this.toPage()
+ },
+
+  computed: {},
+
+  mounted () {},
+
+  methods: {
+    
+  }
+}
+
+</script>
+<style lang='less' scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,9 @@ import Router from 'vue-router'
 
 
 Vue.use(Router)
-const pcMain = () => import('@/components/pc/index.vue')
-const pcHome = () => import('@/components/pc/home.vue')
-const pcProduct = () => import('@/components/pc/product.vue')
-const pcAbout = () => import('@/components/pc/about.vue')
-const pcEvent = () => import('@/components/pc/event.vue')
-const pcHelp = () => import('@/components/pc/help.vue')
 
+
+const toPage = () => import('@/components/mb/toPage.vue')
 const mbMain = () => import('@/components/mb/index.vue')
 const mbHome = () => import('@/components/mb/home.vue')
 const mbProduct = () => import('@/components/mb/product.vue')
@@ -17,37 +13,11 @@ const mbAbout = () => import('@/components/mb/about.vue')
 const mbEvent = () => import('@/components/mb/event.vue')
 const mbHelp = () => import('@/components/mb/help.vue')
 export default new Router({
-  routes: [{
-      path: '/pc',
-      name: 'pcMain',
-      component: pcMain,
-      children: [{
-          path: 'home',
-          name: 'pcHome',
-          component: pcHome,
-        },
-        {
-          path: 'product',
-          name: 'pcProduct',
-          component: pcProduct,
-        },
-        {
-          path: 'about',
-          name: 'pcAbout',
-          component: pcAbout,
-        },
-        {
-          path: 'event',
-          name: 'pcEvent',
-          component: pcEvent,
-        },
-        {
-          path: 'help',
-          name: 'pcHelp',
-          component: pcHelp,
-        },
-
-      ]
+  routes: [
+    {
+      path:'/topage',
+      name:'topage',
+      component: toPage,
     },
     {
       path: '/mb',
